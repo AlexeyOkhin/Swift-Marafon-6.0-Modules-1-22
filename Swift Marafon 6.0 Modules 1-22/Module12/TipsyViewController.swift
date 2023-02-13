36ddb6ebbbbcd6ce9f365d47c93d89ef6e20c1c2
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-13 22:02:37 +0500

    init commit

---
+++ Swift Marafon 6.0 Modules 1-22/Module12/TipsyViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TipsyViewController.swift
+//  Swift Marafon 6.0 Modules 1-22
+//
+//  Created by Djinsolobzik on 13.02.2023.
+//
+
+import UIKit
+
+class TipsyViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    // MARK: - Public Properties
+    // MARK: - Private Properties
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Public Methods
+    // MARK: - IBActions
+    // MARK: - Private Methods
+}
+
+// MARK: - * <- extensions
+
