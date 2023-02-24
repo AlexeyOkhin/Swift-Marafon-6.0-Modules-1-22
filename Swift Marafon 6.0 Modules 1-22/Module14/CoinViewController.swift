ea94f990308d249effc127adc01c10123edb1b69
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-24 15:55:29 +0500

    add coinManager and setting Module 14

--- Swift Marafon 6.0 Modules 1-22/Module14/CoinViewController.swift
+++ Swift Marafon 6.0 Modules 1-22/Module14/CoinViewController.swift
@@ -9,9 +9,40 @@ import UIKit
 
 class CoinViewController: UIViewController {
 
+    private lazy var coinPicker: UIPickerView = {
+        let picker = UIPickerView()
+        picker.translatesAutoresizingMaskIntoConstraints = false
+        return picker
+    }()
+
+    private lazy var coinValueLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var coinTypeLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private lazy var coinImageView: UIImageView = {
+        let imageView = UIImageView()
+        let image = UIImage(systemName: "bitcoinsign.circle.fill")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = image
+        return imageView
+
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemTeal
+    }
+
+    private func setConstraints() {
+        view
     }
 
 }
---
+++ Swift Marafon 6.0 Modules 1-22/Module14/Model/CoinManager.swift
@@ -0,0 +1,18 @@
+//
+//  CoinManager.swift
+//  Swift Marafon 6.0 Modules 1-22
+//
+//  Created by Djinsolobzik on 24.02.2023.
+//
+
+import Foundation
+
+struct CoinManager {
+
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = ""
+
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+
+
+}
