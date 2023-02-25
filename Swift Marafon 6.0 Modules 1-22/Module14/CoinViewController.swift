3cc5dfec8b2b668666f69e1ecbff24aaf7d47e57
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-02-25 19:11:02 +0500

    add network delegate

--- Swift Marafon 6.0 Modules 1-22/Module14/CoinViewController.swift
+++ Swift Marafon 6.0 Modules 1-22/Module14/CoinViewController.swift
@@ -17,12 +17,14 @@ class CoinViewController: UIViewController {
 
     private lazy var coinValueLabel: UILabel = {
         let label = UILabel()
+        label.text = "Value"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
     private lazy var coinTypeLabel: UILabel = {
         let label = UILabel()
+        label.text = "USD"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -38,11 +40,58 @@ class CoinViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setConstraints()
+        coinPicker.delegate = self
+        coinPicker.dataSource = self
         view.backgroundColor = .systemTeal
     }
 
     private func setConstraints() {
-        view
+        let topStack = UIStackView(arrangedSubviews: [coinImageView, coinValueLabel, coinTypeLabel])
+        topStack.axis = .horizontal
+        topStack.distribution = .fillEqually
+        topStack.backgroundColor = .systemGray4
+        topStack.translatesAutoresizingMaskIntoConstraints = false
+        topStack.layer.cornerRadius = 20
+
+        view.addSubview(topStack)
+        view.addSubview(coinPicker)
+
+        NSLayoutConstraint.activate([
+            topStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            topStack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            topStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+//            topStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            topStack.heightAnchor.constraint(equalToConstant: 60),
+
+            coinPicker.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            coinPicker.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            coinPicker.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            //coinPicker.heightAnchor.constraint(equalToConstant: 80)
+        ])
+
     }
 
+}
+
+extension CoinViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        CoinManager().currencyArray.count
+    }
+}
+
+extension CoinViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CoinManager().currencyArray[row]
+    }
+}
+
+extension CoinViewController: CoinServiceDelegate {
+    func didFetchCurse(_ coinService: CoinManager, _ coin: CoinModel) {
+
+    }
 }
--- Swift Marafon 6.0 Modules 1-22/Module14/Model/CoinManager.swift
+++ Swift Marafon 6.0 Modules 1-22/Module14/Model/CoinManager.swift
@@ -7,12 +7,50 @@
 
 import Foundation
 
+protocol CoinServiceDelegate: AnyObject {
+    func didFetchCurse(_ coinService: CoinManager, _ coin: CoinModel)
+}
+
 struct CoinManager {
 
+    weak var delegate: CoinServiceDelegate?
+
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = ""
+    let apiKey = "99DAC5FB-0B79-41C2-9981-E2A2D71CF980"
 
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
+    func fetchCurse(type: String) {
+        let url = "\(baseURL)/\(type)?apikey=\(apiKey)"
+        performRequest(with: url)
+
+    }
+
+
+    private func performRequest(with urlString: String) {
+        let url = URL(string: urlString)!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                if let coin = self.parseJSON(data) {
+                    DispatchQueue.main.async {
+                        self.delegate?.didFetchCurse(self, coin)
+                    }
+                }
+            }
+        }.resume()
+
+    }
+
+    private func parseJSON(_ coin: Data) -> CoinModel? {
+        let decoder = JSONDecoder()
+        let decodeData = try! decoder.decode(CoinModel.self, from: coin)
+
+        let base = decodeData.asset_id_base
+        let quote = decodeData.asset_id_quote
+        let rate = decodeData.rate
+
+        let coin = CoinModel(asset_id_base: base, asset_id_quote: quote, rate: rate)
+        return coin
+    }
 
 }
